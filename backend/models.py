8422f3eab3bff8b7be401befc62856f2a3f52e0e
MicheGigli <84017130+MicheGigli@users.noreply.github.com>
2025-02-17 19:21:45 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -2,16 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Disciplina(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False, unique=True)
+
+class Nazione(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False, unique=True)
+
 class Atleta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    disciplina = db.Column(db.String(100), nullable=False)
-    nazione = db.Column(db.String(100), nullable=False)
+    disciplina_id = db.Column(db.Integer, db.ForeignKey('disciplina.id'), nullable=False)
+    nazione_id = db.Column(db.Integer, db.ForeignKey('nazione.id'), nullable=False)
+
+    disciplina = db.relationship('Disciplina', backref=db.backref('atleti', lazy=True))
+    nazione = db.relationship('Nazione', backref=db.backref('atleti', lazy=True))
 
     def to_dict(self):
         return {
             "id": self.id,
             "nome": self.nome,
-            "disciplina": self.disciplina,
-            "nazione": self.nazione
+            "disciplina": self.disciplina.nome,
+            "nazione": self.nazione.nome
         }
