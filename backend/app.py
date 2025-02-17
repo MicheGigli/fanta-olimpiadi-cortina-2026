24a6ca946ce0d0da73895d50c773e10dbc09c5d0
MicheGigli <84017130+MicheGigli@users.noreply.github.com>
2025-02-17 18:38:15 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configura il database con la variabile d'ambiente
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("postgresql://fantasolimpiadi_db_user:AYA0Fs15FRfKDZQT9mTKSrlyx1FoVCsu@dpg-cupn6m9u0jms73bpnpb0-a.oregon-postgres.render.com/fantasolimpiadi_db")  # Usa la stringa di connessione
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("postgresql://fantasolimpiadi_db_user:AYA0Fs15FRfKDZQT9mTKSrlyx1FoVCsu@dpg-cupn6m9u0jms73bpnpb0-a/fantasolimpiadi_db")  # Usa la stringa di connessione
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
